4d86df53a6827b60c09d39befb75177041e560ee
Ihor Myroniuk <job.ihormyroniuk@gmail.com>
2019-08-20 15:07:04 +0300

    dfsdf

---
+++ AFoundation/AFoundation/AFoundation.h
@@ -0,0 +1,19 @@
+//
+//  AFoundation.h
+//  AFoundation
+//
+//  Created by Ihor Myroniuk on 8/20/19.
+//  Copyright © 2019 Ihor Myroniuk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AFoundation.
+FOUNDATION_EXPORT double AFoundationVersionNumber;
+
+//! Project version string for AFoundation.
+FOUNDATION_EXPORT const unsigned char AFoundationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AFoundation/PublicHeader.h>
+
+
