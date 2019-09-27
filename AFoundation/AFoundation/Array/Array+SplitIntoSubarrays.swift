4f5373155d0147848f1a0cb0edd3816808733df0
Ihor Myroniuk <job.ihormyroniuk@gmail.com>
2019-09-27 15:06:55 +0300

    no message

---
+++ AFoundation/AFoundation/Array/Array+SplitIntoSubarrays.swift
@@ -0,0 +1,17 @@
+//
+//  Array+SplitIntoGroups.swift
+//  AFoundation
+//
+//  Created by Ihor Myroniuk on 9/27/19.
+//  Copyright © 2019 Ihor Myroniuk. All rights reserved.
+//
+
+import Foundation
+
+public extension Array {
+  func splitIntoSubarraysOfSize(_ size: Int) -> [[Element]] {
+    return stride(from: 0, to: count, by: size).map {
+      Array(self[$0 ..< Swift.min($0 + size, count)])
+    }
+  }
+}
