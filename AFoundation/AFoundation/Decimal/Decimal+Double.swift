518506af4d6093bf2dce558a6398d508a61dd0ac
Ihor Myroniuk <job.ihormyroniuk@gmail.com>
2019-10-01 17:29:26 +0300

    no message

---
+++ AFoundation/AFoundation/Decimal/Decimal+Double.swift
@@ -0,0 +1,17 @@
+//
+//  Decimal+Double.swift
+//  AFoundation
+//
+//  Created by Ihor Myroniuk on 10/1/19.
+//  Copyright © 2019 Ihor Myroniuk. All rights reserved.
+//
+
+import Foundation
+
+extension Decimal {
+  
+  var double: Double {
+    return (self as NSNumber).doubleValue
+  }
+  
+}
