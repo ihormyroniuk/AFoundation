c1189445ce5eacc864352cb254347a1065337cc6
Ihor Myroniuk <job.ihormyroniuk@gmail.com>
2019-10-04 13:52:48 +0300

    no message

---
+++ AFoundation/AFoundation/Formatters/Numbers/NumberFormatter+Optionals.swift
@@ -0,0 +1,17 @@
+//
+//  NumberFormatter+Optionals.swift
+//  AFoundation
+//
+//  Created by Ihor Myroniuk on 10/4/19.
+//  Copyright © 2019 Ihor Myroniuk. All rights reserved.
+//
+
+import Foundation
+
+public extension NumberFormatter {
+  
+  func number(from string: String?) -> NSNumber? {
+    return nil
+  }
+  
+}
