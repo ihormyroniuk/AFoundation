ebe9796c644e67fe891850c89d5c371339f5f7ab
Ihor Myroniuk <ihormyroniuk@gmail.com>
2020-10-09 20:46:53 +0300

    no message

---
+++ AFoundationDemo/AppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  AFoundationDemo
+//
+//  Created by Ihor Myroniuk on 09.10.2020.
+//  Copyright © 2020 Ihor Myroniuk. All rights reserved.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+
+}
+
