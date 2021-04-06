1ce00acd850cd98d622307528d3b6a44d6e3e81c
Ihor Myroniuk <ihormyroniuk@gmail.com>
2021-04-06 23:57:36 +0300

    no message

--- AFoundation/Http/HttpExchange.swift
+++ AFoundation/Http/HttpExchange.swift
@@ -11,67 +11,35 @@ import Foundation
 open class HttpExchange<ParsedResponse>: CustomDebugStringConvertible {
     
     open func constructRequest() throws -> HttpRequest {
-        fatalError()
+        throw AFoundationError("\(#function) is not implemented")
     }
     
     open func parseResponse(_ response: HttpResponse) throws -> ParsedResponse {
-        fatalError()
+        throw AFoundationError("\(#function) is not implemented")
     }
+    
+    // MARK: CustomDebugStringConvertible
     
     public var debugDescription: String {
-        return ""
+        return "\(String(reflecting: Self.self))"
     }
 }
-public struct HttpExchangeCannotConstructRequest: Error, CustomDebugStringConvertible {
-    private let fileId: StaticString
-    private let line: UInt
-    private let exchange: Any
-    private let error: Error
-    
-    public init(fileId: StaticString = #fileID, line: UInt = #line, exchange: Any, error: Error) {
-        self.fileId = fileId
-        self.line = line
-        self.exchange = exchange
-        self.error = error
-    }
-    
-    public var debugDescription: String {
-        return "\(fileId):\(String(reflecting: line))\n\(String(reflecting: exchange)) cannot construct request\n\(String(reflecting: error))"
-    }
-}
-public struct HttpExchangeCannotParseResponse: Error, CustomDebugStringConvertible {
-    private let fileId: StaticString
-    private let line: UInt
-    private let exchange: Any
-    private let response: HttpResponse
-    private let error: Error
-    
-    public init(fileId: StaticString = #fileID, line: UInt = #line, exchange: Any, response: HttpResponse, error: Error) {
-        self.fileId = fileId
-        self.line = line
-        self.exchange = exchange
-        self.response = response
-        self.error = error
-    }
-    
-    public var debugDescription: String {
-        return "\(fileId):\(String(reflecting: line))\n\(String(reflecting: exchange)) cannot parse response \n\(String(reflecting: response))\n\(String(reflecting: error))"
-    }
-}
-public struct UnexpectedHttpResponseCodeError: Error, CustomStringConvertible {
 
-    public let code: Int
+open class SchemeHostHttpExchange<ParsedResponse>: HttpExchange<ParsedResponse> {
     
-    public init(code: Int) {
-        self.code = code
+    public let scheme: String
+    public let host: String
+    
+    public init(scheme: String, host: String) {
+        self.scheme = scheme
+        self.host = host
     }
     
-    // MARK: CustomStringConvertible
+    // MARK: CustomDebugStringConvertible
     
-    public var description: String {
-        return ""
+    public override var debugDescription: String {
+        return "\(String(reflecting: Self.self))(scheme: \(String(reflecting: scheme)), hos: \(String(reflecting: host)))"
     }
-    
 }
 
 open class RequestDataHttpExchange<RequestData, ParsedResponse>: HttpExchange<ParsedResponse> {
@@ -82,23 +50,28 @@ open class RequestDataHttpExchange<RequestData, ParsedResponse>: HttpExchange<Pa
         self.requestData = requestData
     }
     
+    // MARK: CustomDebugStringConvertible
+    
     public override var debugDescription: String {
         return "\(String(reflecting: Self.self))(requestData: \(String(reflecting: requestData)))"
     }
 }
 
-open class SchemeHostHttpExchange<RequestData, ParsedResponse>: RequestDataHttpExchange<RequestData, ParsedResponse> {
+open class SchemeHostRequestDataHttpExchange<RequestData, ParsedResponse>: HttpExchange<ParsedResponse> {
     
     public let scheme: String
     public let host: String
+    public let requestData: RequestData
     
     public init(scheme: String, host: String, requestData: RequestData) {
         self.scheme = scheme
         self.host = host
-        super.init(requestData: requestData)
+        self.requestData = requestData
     }
     
+    // MARK: CustomDebugStringConvertible
+    
     public override var debugDescription: String {
-        return "\(String(reflecting: Self.self))(requestData: \(String(reflecting: requestData)), scheme: \(String(reflecting: scheme)), scheme: \(String(reflecting: host)))"
+        return "\(String(reflecting: Self.self))(requestData: \(String(reflecting: requestData)), scheme: \(String(reflecting: scheme)), host: \(String(reflecting: host)))"
     }
 }
