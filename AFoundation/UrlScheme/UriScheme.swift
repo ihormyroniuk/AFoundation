e2d3558dcbe2a37e9e7cff7f09425d2e62b360f6
Ihor Myroniuk <ihormyroniuk@gmail.com>
2021-04-28 23:12:16 +0300

    no message

--- AFoundation/UrlScheme/UriScheme.swift
+++ AFoundation/UrlScheme/UriScheme.swift
@@ -11,7 +11,7 @@ import Foundation
 /**
     Implemented based on https://www.iana.org/assignments/uri-schemes/uri-schemes.xhtml
  */
-public enum UrlScheme {
+public enum UriScheme {
     
     public static let http = "http"
     public static let https = "https"
