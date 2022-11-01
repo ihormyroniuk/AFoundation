3917dfa82a36e5fe1f27c09c8169dd56d6022449
Ihor Myroniuk <ihormyroniuk@gmail.com>
2022-11-01 17:36:27 +0200

    no message

--- AFoundationUnitTesting/Decimal/DecimalRoundUnitTesting.swift
+++ AFoundationUnitTesting/Decimal/DecimalRoundUnitTesting.swift
@@ -3,17 +3,22 @@ import XCTest
 
 class DecimalRoundUnitTesting: XCTestCase {
     
-    func testDouble() {
-        let double = Double(15678907) + Double(66886)
-        let decimal = Decimal(double)
+    func testRound() {
+        let decimal = Decimal(string: "5.83776")
         
-        let decimalDouble: Double
-        do { decimalDouble = try decimal.double() } catch {
-            XCTFail("Unexpected error \(error.self) is thrown")
-            return
-        }
+        let roundedDecimal = decimal?.rounded(3, .bankers)
         
-        XCTAssertTrue(decimalDouble == double, "Unexpected \(decimalDouble)) is returned, but \(double) is expected")
+        let expectedRoundedDecimal = Decimal(string: "5.838")
+        XCTAssertTrue(roundedDecimal == expectedRoundedDecimal, "Unexpected \(String(describing: roundedDecimal)) is returned, but \(String(describing: expectedRoundedDecimal)) is expected")
+    }
+    
+    func testRounded() {
+        var decimal = Decimal(string: "5.83776")
+        
+        decimal?.round(3, .bankers)
+        
+        let expectedDecimal = Decimal(string: "5.838")
+        XCTAssertTrue(decimal == expectedDecimal, "Unexpected \(String(describing: decimal)) is returned, but \(String(describing: expectedDecimal)) is expected")
     }
     
 }
