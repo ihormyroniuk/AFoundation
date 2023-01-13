a4c30a47b02814ccca7565888aae683019728c79
Ihor Myroniuk <ihormyroniuk@gmail.com>
2023-01-14 01:28:58 +0200

    no message

--- AFoundation/Locale/LocaleComponents.swift
+++ AFoundation/Locale/LocaleComponents.swift
@@ -1,25 +1,25 @@
-import Foundation
-
-@available(macOS 13, iOS 16, tvOS 16, watchOS 9, *)
-public extension Locale.Components {
-    
-    // MARK: - Initialization
-    
-    init(language: AFoundation.Language, script: AFoundation.Script?, region: AFoundation.Region?) {
-        let localeLanguageCode = Locale.LanguageCode(language.code)
-        let localeScript: Locale.Script?
-        if let scriptCode = script?.code {
-            localeScript = Locale.Script(scriptCode)
-        } else {
-            localeScript = nil
-        }
-        let localeRegion: Locale.Region?
-        if let regionCode = region?.code {
-            localeRegion = Locale.Region(regionCode)
-        } else {
-            localeRegion = nil
-        }
-        self.init(languageCode: localeLanguageCode, script: localeScript, languageRegion: localeRegion)
-    }
-    
-}
+//import Foundation
+//
+//@available(macOS 13, iOS 16, tvOS 16, watchOS 9, *)
+//public extension Locale.Components {
+//    
+//    // MARK: - Initialization
+//    
+//    init(language: AFoundation.Language, script: AFoundation.Script?, region: AFoundation.Region?) {
+//        let localeLanguageCode = Locale.LanguageCode(language.code)
+//        let localeScript: Locale.Script?
+//        if let scriptCode = script?.code {
+//            localeScript = Locale.Script(scriptCode)
+//        } else {
+//            localeScript = nil
+//        }
+//        let localeRegion: Locale.Region?
+//        if let regionCode = region?.code {
+//            localeRegion = Locale.Region(regionCode)
+//        } else {
+//            localeRegion = nil
+//        }
+//        self.init(languageCode: localeLanguageCode, script: localeScript, languageRegion: localeRegion)
+//    }
+//    
+//}
